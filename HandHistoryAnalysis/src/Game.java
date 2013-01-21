33dfedf1302973cd3d7d2ee2c65dcc7a30f81d3c
araju <akhilraju@gmail.com>
2013-01-20 21:26:28 -0500

    update hand history analyzer to calc equity and pfr

--- HandHistoryAnalysis/src/Game.java
+++ HandHistoryAnalysis/src/Game.java
@@ -17,18 +17,31 @@ public class Game {
 		POSTRIVER
 	}
 	
+	private String opponent;
 	private GameState gState;
 	private String winner;
+	private int winAmount;
+	private Card[] hand;
+	private EquityCalculator ec;
+	private double preFlopEquity;
+	private double postFlopEquity;
+	private boolean pfr;
 	
 	private final String FLOP = "*** FLOP ***";
 	private final String TURN = "*** TURN ***";
 	private final String RIVER = "*** RIVER ***";
 	private final String WIN = "wins the pot";
+	private final String DEALT = "Dealt";
 	
-	public Game(ArrayList<String> gHist) {
+	public Game(ArrayList<String> gHist, String op) {
+		opponent = op;
 		gameHist = gHist;
 		gState = GameState.PREFLOP;
 		winner = "";
+		winAmount = 0;
+		preFlopEquity = -1;
+		postFlopEquity = -1;
+		pfr = false;
 		processHistory();
 	}
 	
@@ -39,18 +52,69 @@ public class Game {
 	}
 	
 	private void processLine(String line) {
+		//System.out.println("here!!");
 		if (line.length() == 0) return;
+		//System.out.println("Line: " + line);
 		if (line.charAt(0) == '*') {
 			changeGameState(line);
 		} else if (line.contains(WIN)) {
+			//System.out.println("got here 2");
 			processWinner(line);
+		} else if (line.contains(DEALT)) {
+			//System.out.println("got here");
+			processHand(line);
+		} else if (line.contains(opponent + " raises") &&
+				gState == GameState.PREFLOP) {
+			int r = 0;
+			try {
+				String[] tokens = line.split(" ");
+				r = Integer.parseInt(tokens[tokens.length - 1]);
+			} catch (Exception e) {
+				pfr = true;
+			}
+			if (r >= 11) {
+				pfr = true;
+			}
 		}
 		
+	}
+	
+	private void processHand(String line) {
+		String[] tokens = line.split(" ");
+		hand = new Card[] {
+				CardUtils.getCardByString(tokens[3].substring(1)),
+				CardUtils.getCardByString(tokens[4]),
+				CardUtils.getCardByString(tokens[5].substring(0,2))
+		};
+		ec = new EquityCalculator(hand, null);
+		preFlopEquity = ec.calculateTotalEquity();
+	}
+	
+	public boolean pfr() {
+		return pfr;
+	}
+	
+	public double getPreFlopEquity() {
+		return preFlopEquity;
+	}
+	
+	public double getPostFlopEquity() {
+		return postFlopEquity;
 	}
 	
 	private void changeGameState(String line) {
 		if (line.contains(FLOP)) {
 			gState = GameState.PRETURN;
+			String[] tokens = line.split(" ");
+			int len = tokens.length;
+			ec.setBoard(new Card[] {
+					CardUtils.getCardByString(tokens[len - 3].substring(1)),
+					CardUtils.getCardByString(tokens[len - 2]),
+					CardUtils.getCardByString(tokens[len - 1].substring(0, 2)),
+					null,
+					null
+			});
+			postFlopEquity = ec.calculateTotalEquity();
 		} else if (line.contains(TURN)) {
 			gState = GameState.PRERIVER;
 		} else if (line.contains(RIVER)) {
@@ -59,7 +123,15 @@ public class Game {
 	}
 	
 	private void processWinner(String line) {
-		winner = line.split(" ")[0];
+		String[] tokens = line.split(" ");
+		winner = tokens[0];
+		String amt = tokens[tokens.length - 1];
+		try {
+			winAmount = Integer.parseInt(amt.substring(1, amt.length() - 1));
+		} catch (Exception e) {
+			System.out.println("Error parsing win amount");
+		}
+		
 	}
 	
 	public String getWinner() {
@@ -69,6 +141,10 @@ public class Game {
 	//returns what round of betting the game ends on
 	public GameState getFinalRound() {
 		return gState;
+	}
+	
+	public int getWinAmount() {
+		return winAmount;
 	}
 	
 	
--- HandHistoryAnalysis/src/Match.java
+++ HandHistoryAnalysis/src/Match.java
@@ -16,6 +16,18 @@ public class Match {
 	private int[] roundCount; //counts how many hands end in each of the rounds of betting (pre-flop, etc.)
 	private int[] winCount; //count how many times we win in each round
 	
+	private int[] preFlopEquityHist; //how many hands had this equity that end pre-flop
+	private int[] preFlopEquityWinsHist; //histogram showing how many games we win pre-flop for different equity ranges
+	private int[] preFlopEquityWinAmts; //how much we win (net) pre-flop with different equity
+	private int[] postFlopEquityHist; //histogram for how many hands have this equity that end post-flop
+	private int[] postFlopEquityWinsHist; //how many wins post-flop for different equities
+	private int[] postFlopEquityWinAmts; //net win amount for diff equities for games ending post-flop
+	private int[] totalWinsPreEquity; //histogram by pre-flop equity for wins
+	private int[] totalHandsPreEquity; //how many hands of different pre-flop equity were played
+	private int[] totalAmtPreEquity; //net amt won by pre-flop equity
+	
+	private int pfrCount;
+	
 	public Match(String m, String op, String f) {
 		opponent = op;
 		me = m;
@@ -23,6 +35,16 @@ public class Match {
 		gameCount = 0;
 		roundCount = new int[4];
 		winCount = new int[4];
+		preFlopEquityHist = new int[10];
+		preFlopEquityWinAmts = new int[10];
+		preFlopEquityWinsHist = new int[10];
+		postFlopEquityHist = new int[10];
+		postFlopEquityWinsHist = new int[10];
+		postFlopEquityWinAmts = new int[10];
+		totalWinsPreEquity = new int[10];
+		totalHandsPreEquity = new int[10];
+		totalAmtPreEquity = new int[10];
+		pfrCount = 0;
 	}
 	
 	public void processMatchHistory() {
@@ -31,32 +53,23 @@ public class Match {
 		try {
 			br = new BufferedReader(new FileReader(fileName));
 			String line = br.readLine();
+			line = br.readLine();
+			line = br.readLine();
 			//System.out.println(line);
 			//System.out.println("Hi" + (line == null));
 			ArrayList<String> game = new ArrayList<String>();
 			while (line != null) {
+				//System.out.println(line);
+				if (line.startsWith("6.S912")) {
+					line = br.readLine();
+					continue;
+				}
 				if (!line.startsWith("Hand #")) {
 					game.add(line);
 				} else {
 					//System.out.println("Got here " + opponent);
-					Game g = new Game(game);
-					String winner = g.getWinner();
-					boolean won = winner.equals(me);
-					int i = 0;
-					switch(g.getFinalRound()) {
-					case PREFLOP:
-						i = 0; break;
-					case PRETURN:
-						i = 1; break;
-					case PRERIVER:
-						i = 2; break;
-					case POSTRIVER:
-						i = 3; break;
-					}
-					roundCount[i]++;
-					if (won) {
-						winCount[i]++;
-					}
+					Game g = new Game(game, opponent);
+					processGame(g);
 					game = new ArrayList<String>();
 					gameCount++;
 				}
@@ -65,6 +78,68 @@ public class Match {
 			br.close();
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+	
+	private void processGame(Game g) {
+		String winner = g.getWinner();
+		boolean won = winner.equals(me);
+		int i = 0;
+		switch(g.getFinalRound()) {
+		case PREFLOP:
+			i = 0; 
+			processPreFlopGame(g, won);
+			break;
+		case PRETURN:
+			i = 1; 
+			processPostFlopGame(g, won);
+			break;
+		case PRERIVER:
+			i = 2; 
+			processPostFlopGame(g, won);
+			break;
+		case POSTRIVER:
+			i = 3; 
+			processPostFlopGame(g, won);
+			break;
+		}
+		double equity = g.getPreFlopEquity();
+		int index = (int)(equity * 10);
+		totalHandsPreEquity[index]++;
+		roundCount[i]++;
+		if (won) {
+			totalWinsPreEquity[index]++;
+			totalAmtPreEquity[index]+=g.getWinAmount();
+			winCount[i]++;
+		} else {
+			totalAmtPreEquity[index] -= g.getWinAmount();
+		}
+		if (g.pfr()) {
+			pfrCount++;
+		}
+	}
+	
+	private void processPreFlopGame(Game g, boolean won) {
+		double equity = g.getPreFlopEquity();
+		int index = (int)(equity * 10);
+		preFlopEquityHist[index]++;
+		if (won) {
+			preFlopEquityWinsHist[index]++;
+			preFlopEquityWinAmts[index] += g.getWinAmount();
+		} else {
+			preFlopEquityWinAmts[index] -= g.getWinAmount();
+		}
+	}
+	
+	private void processPostFlopGame(Game g, boolean won) {
+		double equity = g.getPostFlopEquity();
+		int index = (int)(equity * 10);
+		postFlopEquityHist[index]++;
+		if (won) {
+			postFlopEquityWinsHist[index]++;
+			postFlopEquityWinAmts[index] += g.getWinAmount();
+		} else {
+			postFlopEquityWinAmts[index] -= g.getWinAmount();
 		}
 	}
 	
@@ -85,6 +160,70 @@ public class Match {
 				((double)roundCount[2]) / gameCount,
 				((double)roundCount[3]) / gameCount
 		};
+	}
+	
+	public double[] getWinPercentageByPreEquityHistogram() {
+		double[] hist = new double[10];
+		for (int i = 0; i < totalWinsPreEquity.length; i++) {
+			hist[i] = ((double)totalWinsPreEquity[i]) / (totalHandsPreEquity[i] == 0 ? 1 : totalHandsPreEquity[i]);
+		}
+		return hist;
+	}
+	
+	public int[] getPreEquityWinCountPreFlop() {
+		return preFlopEquityWinsHist;
+	}
+	
+	public int[] getPreEquityHandCountPreFlop() {
+		return preFlopEquityHist;
+	}
+	
+	public int[] getWinCountByPreEquityHistogram() {
+		return totalWinsPreEquity;
+	}
+	
+	public int[] getNetAmtWonByPreEquityHistogram() {
+		return totalAmtPreEquity;
+	}
+	
+	public int[] getTotalCountByPreEquityHistogram() {
+		return totalHandsPreEquity;
+	}
+	
+	public double[] getPreFlopWinPercentageByEquity() {
+		double[] hist = new double[10];
+		for (int i = 0; i < preFlopEquityWinsHist.length; i++) {
+			hist[i] = ((double)preFlopEquityWinsHist[i]) / (preFlopEquityHist[i] == 0 ? 1 : preFlopEquityHist[i]);
+		}
+		return hist;
+	}
+	
+	public int[] getPreFlopNetWinAmtByEquity() {
+		return preFlopEquityWinAmts;
+	}
+	
+	public double[] getPostFlopWinPercentageByPostEquity() {
+		double[] hist = new double[10];
+		for (int i = 0; i < postFlopEquityWinsHist.length; i++) {
+			hist[i] = ((double)postFlopEquityWinsHist[i]) / (postFlopEquityHist[i] == 0 ? 1 : postFlopEquityHist[i]);
+		}
+		return hist;
+	}
+	
+	public int[] getPostFlopNetWinningsByPostEquity() {
+		return postFlopEquityWinAmts;
+	}
+	
+	public int[] getPostFlopPostEquityWinCount() {
+		return postFlopEquityWinsHist;
+	}
+	
+	public int[] getPostFlopPostEquityHandCount() {
+		return postFlopEquityHist;
+	}
+	
+	public double pfr() {
+		return ((double)pfrCount) / gameCount;
 	}
 	
 	public int getGameCount() {
